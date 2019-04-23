e819c2e1763459b1c7ec2907775548ba74211789
imtiaz <programmer.imtiaz@gmail.com>
2019-04-24 01:34:49 +0600

    ADD- countdown added ADD- Game result showing added

--- app/src/main/java/com/example/kotlintapme/MainActivity.kt
+++ app/src/main/java/com/example/kotlintapme/MainActivity.kt
@@ -2,13 +2,58 @@ package com.example.kotlintapme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var score : Int = 0
+    internal var gameStarted : Boolean = false
+    internal lateinit var countDownTimer : CountDownTimer
+    internal var initialCountDown : Long = 10000
+    internal var countDownInterval : Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resetCountDown()
+        btnTapMe.setOnClickListener {
+            incrementScore()
+        }
     }
 
+    private fun resetCountDown(){
+        score = 0
+        gameStarted = false
 
+        yourScore.text = getString(R.string.your_score, score.toString())
+        val initalTimeLeft = initialCountDown / 1000
+        tvTimeLeft.text = getString(R.string.time_left, initalTimeLeft.toString())
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval){
+            override fun onFinish() {
+                gameOver()
+            }
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000 ;
+                tvTimeLeft.text = getString(R.string.time_left, timeLeft.toString())
+            }
+        }
+    }
+
+    private fun gameOver(){
+        Toast.makeText(this, getString(R.string.your_final_score, score.toString()), Toast.LENGTH_LONG).show()
+        resetCountDown()
+    }
+
+    private fun incrementScore(){
+        if(!gameStarted){
+            countDownTimer.start()
+            gameStarted = true
+        }
+        score ++
+        val newScore = getString(R.string.your_score, score.toString())
+        yourScore.text = newScore
+    }
 }
